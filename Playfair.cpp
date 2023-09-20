a9d710ee27102db285a584a535ac823fce26d7e2
Farrukh Riaz <115878714+deviant101@users.noreply.github.com>
2023-09-20 23:58:30 +0500

    Playfair Cipher Encryption

--- Playfair.cpp
+++ Playfair.cpp
@@ -25,9 +25,26 @@ int main(){
 
     return 0;
 }
+int findloc(char** m, char ch)
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            if(ch == m[i][j])
+            {
+                return (i*10)+j;
+            }
+        }
+    }
+    return 0;
+}
 void Playfair(string plain, string kword){
 
-    char Matrix[5][5];
+    char **Matrix;
+    Matrix = new char*[5];
+    for(int i=0; i<5; i++)
+        Matrix[i]= new char[5];
     // int strlen=strLen(kword);
     int count=0;
     int alphabets=97;
@@ -52,29 +69,49 @@ void Playfair(string plain, string kword){
         cout<<endl;
     }
     string temp="";
-    for(int i=0; i<strLen(plain); i+=2){
-        int positions[2][2];
-        for(int j=0; j<5; ++j){
-            for(int k=0; k<5; ++k){
-                if(plain[i]==Matrix[i][j]){
-                    positions[0][0]=i;  //row in 0 index
-                    positions[0][1]=j;  //col in 1 index
-                }
-                else if(plain[i+1]==Matrix[i][j]){
-                    positions[1][0]=i;  //row in 0 index
-                    positions[1][1]=j;  //col in 1 index
-                }
-            }
+    string pair[strLen(plain)/2];
+    for(int i=0,j=0;i<strLen(plain);i+=2,j++)
+    {
+        pair[j]+=plain[i];
+        pair[j]+=plain[i+1];
+    }
+    string cypher="";
+    for(int i=0;i<strLen(plain)/2;i++)
+    {
+        temp=pair[i];
+        int r1=findloc(Matrix,temp[0])/10;
+        int c1=findloc(Matrix,temp[0])%10;
+        int r2=findloc(Matrix,temp[1])/10;
+        int c2=findloc(Matrix,temp[1])%10;
+        if(r1 == r2)
+        {
+            if(c1 == 4)
+                cypher+=Matrix[r1][0];
+            else
+                cypher+=Matrix[r1][c1+1];
+            if(c2 == 4)
+                cypher+=Matrix[r1][0];
+            else
+                cypher+=Matrix[r1][c2+1];
         }
-        if(positions[0][0]==positions[1][0]){ //present in same row
-            temp+=
+        else if(c1 == c2)
+        {
+            if(r1 == 4)
+                cypher+=Matrix[0][c1];
+            else
+                cypher+=Matrix[r1+1][c1];
+            if(r2 == 4)
+                cypher+=Matrix[0][c2];
+            else
+                cypher+=Matrix[r2+1][c2];
         }
-        else if(positions[0][1]==positions[1][1]){  //present in same column
-
+        else
+        {
+            cypher+=Matrix[r1][c2];
+            cypher+=Matrix[r2][c1];
         }
     }
-    
-
+    cout<<cypher<<endl;
 }
 
 string checkKeyword(string str){
@@ -97,21 +134,30 @@ string checkKeyword(string str){
 string checkPlainText(string str){
 
     string temp="";
+    
     for(int i=0; i<strLen(str); ++i){
-        if(i%2==0){
             if(str[i]==str[i+1]){
                 temp+=str[i];
                 temp+='x';
             }
             else
+            {
                 temp+=str[i];
-        }
-        else
-            temp+=str[i];
+                temp+=str[i+1];
+                i++;
+            }
     }
     if(strLen(temp)%2==1)   //to make string length even by adding x at end
         temp+='x';
-    return temp;
+    string temp2="";
+    for(int i=0;i<temp.length();i++)
+    {
+        if(temp[i]==0)
+            continue;
+        else
+            temp2+=temp[i];
+    }
+    return temp2;
 }
 
 bool checkExistence(char ch, string str){
